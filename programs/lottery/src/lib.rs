129998ffb1e323c8ae964208f3cc9f4d4ede8b06
juancolchete <juanudkp@gmail.com>
2024-09-08 20:13:49 +0000

    feat!: new layout handle

--- programs/lottery/src/lib.rs
+++ programs/lottery/src/lib.rs
@@ -11,13 +11,27 @@ pub mod lottery {
         Ok(())
     }
 
-    pub fn set(ctx: Context<Set>, key1: u64, key2: u64, val: u64) -> Result<()> {
-        for i in 0..ctx.accounts.val.value.len() as usize {
-          if ctx.accounts.val.value[i] == 0{
-            ctx.accounts.val.value[i] = val;
-            break;
-          }
-        }
+    pub fn set(ctx: Context<Set>,key1: u64, key2: u64, ticket_number: u64) -> Result<()> {
+        // if ticket_number > 999999 && ticket_number < 2000000 {
+        //     for i in 0..ctx.accounts.lottery_info.tickets.len() as usize {
+        //         if ctx.accounts.lottery_info.tickets[i].number == 0 {
+        //             ctx.accounts.lottery_info.tickets[i].number = ticket_number;
+        //         }
+        //         if ctx.accounts.lottery_info.tickets[i].number == ticket_number {
+        //             for b in 0..ctx.accounts.lottery_info.tickets[i].buyers.len() as usize {
+        //                 if ctx.accounts.lottery_info.tickets[i].buyers[b]
+        //                     == ctx.accounts.lottery_info.default
+        //                 {
+        //                     ctx.accounts.lottery_info.tickets[i].buyers[b] = ctx.accounts.signer.key(); 
+        //                     break;
+        //                 }
+        //             }
+        //             break;
+        //         }
+        //     }
+        // }else{
+        //     return err!(LotteryError::WrongNumber);
+        // }
         Ok(())
     }
 }
@@ -27,10 +41,10 @@ pub mod lottery {
 pub struct Initialize<'info> {
     #[account(init,
               payer = signer,
-              space = size_of::<Val>() + 8,
+              space = size_of::<LotteryInfo>() + 8,
               seeds=[&key1.to_le_bytes().as_ref(),&key2.to_le_bytes().as_ref()],
               bump)]
-    val: Account<'info, Val>,
+    lottery_info: Account<'info, Ticket>,
 
     #[account(mut)]
     signer: Signer<'info>,
@@ -42,14 +56,25 @@ pub struct Initialize<'info> {
 #[instruction(key1: u64,key2: u64)]
 pub struct Set<'info> {
     #[account(mut)]
-    val: Account<'info, Val>,
+    lottery_info: Account<'info, LotteryInfo>,
     #[account(mut)]
     signer: Signer<'info>,
 }
 
 #[account]
-// #[derive(Default)]
-pub struct Val {
-    value: [u64; 50],
-    index: u64,
+pub struct LotteryInfo {
+    default: Pubkey,
+    tickets: [Ticket; 1000],
+    full_match: [Pubkey; 500],
+    partial_match: [Pubkey; 500],
+}
+#[error_code]
+pub enum LotteryError {
+    #[msg("Number specified is in a wrong range")]
+    WrongNumber,
+}
+#[account]
+pub struct Ticket {
+    buyers: [Pubkey; 50],
+    number: u64,
 }
--- tests/lottery.ts
+++ tests/lottery.ts
@@ -31,8 +31,8 @@ describe("lottery", () => {
 
     const keyOne = new anchor.BN(1);
     const keyTwo = new anchor.BN(5);
-    const valDefault = new anchor.BN(8);
-    const valSigner = new anchor.BN(9);
+    const valDefault = new anchor.BN(1000000);
+    const valSigner = new anchor.BN(1999999);
 
     const seeds = [key1.toArrayLike(Buffer, "le", 8), key2.toArrayLike(Buffer, "le", 8)];
     let valueAccount = anchor.web3.PublicKey.findProgramAddressSync(
@@ -41,25 +41,25 @@ describe("lottery", () => {
     )[0];
 
     await program.methods.initialize(key1, key2).accounts(valueAccount).rpc()
-    await program.methods.set(key1, key2, val).accounts({ val: valueAccount }).rpc()
+    await program.methods.set(key1,key2,valSigner).accounts({ lotteryInfo: valueAccount }).rpc()
 
 
 
-    const seedsTwo = [keyOne.toArrayLike(Buffer, "le", 8), keyTwo.toArrayLike(Buffer, "le", 8)];
-    let valueAccountTwo = anchor.web3.PublicKey.findProgramAddressSync(
-      seedsTwo,
-      program.programId
-    )[0];
+    // const seedsTwo = [keyOne.toArrayLike(Buffer, "le", 8), keyTwo.toArrayLike(Buffer, "le", 8)];
+    // let valueAccountTwo = anchor.web3.PublicKey.findProgramAddressSync(
+    //   seedsTwo,
+    //   program.programId
+    // )[0];
 
-    await program.methods.initialize(keyOne, keyTwo).accounts({signer:newKeypair.publicKey,val:valueAccountTwo} as any).signers([newKeypair]).rpc()
-    await program.methods.set(keyOne, keyTwo, valDefault).accounts({val: valueAccountTwo }).rpc()
-    await program.methods.set(keyOne, keyTwo, valSigner).accounts({signer:newKeypair.publicKey, val: valueAccountTwo }).signers([newKeypair]).rpc()
+    // await program.methods.initialize(keyOne, keyTwo).accounts({signer:newKeypair.publicKey,lottery_info:valueAccountTwo} as any).signers([newKeypair]).rpc()
+    // await program.methods.set(keyOne,keyTwo,valDefault).accounts({lotteryInfo: valueAccountTwo }).rpc()
+    // await program.methods.set(keyOne,keyTwo,valSigner).accounts({signer:newKeypair.publicKey, lotteryInfo: valueAccountTwo }).signers([newKeypair]).rpc()
 
-    let result = await program.account.val.fetch(valueAccount)
+    // let result = await program.account.lotteryInfo.fetch(valueAccount)
 
-    console.log("result: ", result.value, valueAccount.toBase58())
-    result = await program.account.val.fetch(valueAccountTwo)
+    // console.log("result: ", result, valueAccount.toBase58())
+    // result = await program.account.lotteryInfo.fetch(valueAccountTwo)
 
-    console.log("result: ", result.value, valueAccountTwo.toBase58())
+    // console.log("result: ", result, valueAccountTwo.toBase58())
   });
 });
